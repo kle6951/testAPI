eb4cbdf66db4b710ec7c0151228b37fa24038eb6
Khanh Le <lekhanhsone@gmail.com>
2024-11-17 20:39:11 -0700

    Update path name

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,16 @@
 "use strict";
 
 module.exports.register = (app, database) => {
+  app.get("/", async (req, res) => {
+    res.status(200).send("This is running!").end();
+  });
 
-    app.get('/', async (req, res) => {
-        res.status(200).send("This is running!").end();
-    });
+  app.get("/api/resources", async (req, res) => {
+    let query;
+    query = database.query("SELECT * FROM Listings");
 
-    app.get('/api/resources', async (req, res) => {
-        let query;
-        query = database.query('SELECT * FROM listings');
+    const records = await query;
 
-        const records = await query;
-
-        res.status(200).send(JSON.stringify(records)).end();
-    });
-
+    res.status(200).send(JSON.stringify(records)).end();
+  });
 };
