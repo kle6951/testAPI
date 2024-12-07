eac7284b727f3ef961d340ffd126e2503b307b34
Khanh Le <lekhanhsone@gmail.com>
2024-12-07 00:47:01 -0700

    New update - Include user information and listing count in get function for RoomateListings route

--- src/routes/roommateListings.js
+++ src/routes/roommateListings.js
@@ -7,12 +7,25 @@ module.exports.register = (app, database) => {
 
   // GET
   app.get("/api/roomateListings", async (req, res) => {
-    let query;
-    query = database.query("SELECT * FROM RoomateListings");
+    try {
+      const query = `
+        SELECT 
+          RoomateListings.*, 
+          Users.full_name AS user_name, 
+          Users.email AS user_email, 
+          Users.avatar AS user_avatar,
+          (SELECT COUNT(*) FROM RoomateListings AS rl WHERE rl.user_id = Users.id) AS user_roomate_listing_count
+        FROM RoomateListings
+        JOIN Users ON RoomateListings.user_id = Users.id
+      `;
 
-    const records = await query;
+      const records = await database.query(query);
 
-    res.status(200).send(JSON.stringify(records)).end();
+      res.status(200).send(JSON.stringify(records)).end();
+    } catch (error) {
+      console.error("Error fetching roomate listings:", error.message);
+      res.status(500).send("Error fetching roomate listings").end();
+    }
   });
 
   // POST
