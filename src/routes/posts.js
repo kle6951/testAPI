463dd676d6f8a370747b5f7738651e58ff48aa66
Khanh Le <lekhanhsone@gmail.com>
2024-12-08 00:58:59 -0700

    Fixing emoji issue

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -12,10 +12,11 @@ module.exports.register = (app, database) => {
           Users.avatar AS user_avatar
         FROM Posts
         JOIN Users ON Posts.user_id = Users.id
-        `;
+      `;
       const records = await database.query(query);
 
-      res.status(200).send(JSON.stringify(records)).end();
+      res.setHeader("Content-Type", "application/json; charset=utf-8"); // Set UTF-8 encoding
+      res.status(200).json(records); // Automatically sets correct headers and encodes as JSON
     } catch (error) {
       console.error("Error fetching listings:", error.message);
       res.status(500).send("Error fetching listings").end();
