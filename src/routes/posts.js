3ed031ce5f24f82094a1c8b605945c49990dbd49
Khanh Le <lekhanhsone@gmail.com>
2024-12-08 02:41:47 -0700

    Added POST function for posts route

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -3,6 +3,7 @@ module.exports.register = (app, database) => {
   app.get("/", async (req, res) => {
     res.status(200).send("This is running!").end();
   });
+  // GET
   app.get("/api/posts", async (req, res) => {
     try {
       const query = `
@@ -19,6 +20,28 @@ module.exports.register = (app, database) => {
     } catch (error) {
       console.error("Error fetching listings:", error.message);
       res.status(500).send("Error fetching listings").end();
+    }
+  });
+  // POST
+  app.post("/api/posts", async (req, res) => {
+    try {
+      const { caption, user_id, images } = req.body;
+      if (!caption || !user_id || !images) {
+        return res.status(400).send("Missing required fields").end();
+      }
+      const query = `
+        INSERT INTO POSTS (caption, user_id, images)
+        VALUES (?,?,?)
+      `;
+
+      const result = await database.query(query, [caption, user_id, images]);
+      res
+        .status(201)
+        .send({ message: "Posts created successfully", id: result.insertId })
+        .end();
+    } catch (error) {
+      console.error("Error creating post:", error.message);
+      res.status(500).send("Error creating post").end();
     }
   });
 };
